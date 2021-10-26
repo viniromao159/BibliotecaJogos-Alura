7c074d263fa347b9e418ebabe1a711e5ae9cece2
Vinicius Lopes R <vini.romao159@gmail.com>
2021-10-26 18:05:19 -0300

    Commit initial

---
+++ jogos/forca.py
@@ -0,0 +1,15 @@
+def jogar_forca():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+
+
+
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar_forca()
